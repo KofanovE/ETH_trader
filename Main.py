7953d4fa4c1acc340b657bb33c82b26bd4fbbeea
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-10-29 22:12:56 +0300

    commit_1_2 Створення функції, що формує повний датафрейм

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 """
-В результаті отримано тестовий датафрейм з цінами барів та графік мін, макс та закриття барів
+В PrepareDF функції планується добавити стовпці мін та макс каналу з середніх значеннь з історії, 
+позиція в каналі та кут нахилу тренду
 """
 def main():
     api_key = 'LSZH501HZPZVU1GA'
@@ -13,11 +14,23 @@ def main():
 
     url = f'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol={symbol}&market=USD&interval={interval_var}&apikey={api_key}&datatype=csv&outputsize=full'
     df = pd.read_csv(url)
-    df = df[::-1]
-    frame = pd.DataFrame(df[['high', 'low', 'close']])  # Тестовий датафрейм
-    print(frame)
-    frame[['high', 'low']].plot()
-    plt.show()
+    df = df[::-1]                                       # [timestamp, open, high, low, close, volume]
+    #frame = pd.DataFrame(df[['high', 'low', 'close']])  # Тестовий датафрейм
+    #print(df)
+    #print(frame)
+    #frame[['high', 'low']].plot()
+    #plt.show()
+    PrepareDF(df)
+
+
+
+
+def PrepareDF(DF):                                                     # Функція формування повного датафрейму
+    ohlc = DF
+    ohlc.columns = ["date", "open", "high", "low", "close", "volume"]  # Зміна назв колонок датафрейму
+    ohlc = ohlc.set_index('date')
+    print(ohlc)
+
 
 
 
