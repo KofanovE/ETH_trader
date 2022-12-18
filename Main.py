d7be89da19deeadc58c09b1bac87618792241725
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2022-12-18 20:26:07 +0200

    commit_1_9_0 Develop added same  hand_functions: 1.get_futures_klines 2.open_position 3.close_position 4.get_opened_positions 5.check_and_close_orders 6.get_symbol_price

--- Main.py
+++ Main.py
@@ -24,22 +24,118 @@ def main():
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_colwidth', None)
 
-    # api_key = "8T4KQAIG4PFXRYSG"
-    # api_key = 'LSZH501HZPZVU1GA'
-    # interval_var = '15min'
-    # symbol = 'BTC'
+    client = Client(KEY, SECRET)
+    symbol = 'ETHUSDT'
 
-    # url = f'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol={symbol}&market=USD&interval={interval_var}&apikey={api_key}&datatype=csv&outputsize=full'
-    url = f'https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=ETH&market=USD&interval=5min&apikey=demo&datatype=csv'
-    df = pd.read_csv(url)
-    df = df[::-1]                                       # [timestamp, open, high, low, close, volume]
-    prepared_df = PrepareDF(df)
+    def get_futures_klines(symbol, limit=500):   #example symple request to binance and get last price
+        x = requests.get("nttps://binance.com/fapi/v1/klines?symbol='+symbol+'&limit='+str(limit)+'&interval=5m")
+        df = pd.DataFrame(x.json())
+        df.columns = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'di', 'd2', 'd3', 'd4', 'd5']
+        df = df.drop(['d1', 'd2', 'd3', 'd4', 'd5'], axis=1)
+        df['open'] = df['open'].astype(float)
+        df['high'] = df['high'].astype(float)
+        df['low'] = df['low'].astype(float)
+        df['close'] = df['close'].astype(float)
+        df['volume'] = df['volume'].astype(float)
+        return df
+
+    def open_position(symbol, s_l, quantity_l): #fincion opening position
+        sprice = get_symbol_price(symbol)
+
+        if s_l == 'long':
+            close_price = str(round(sprice * (1 + 0.01), 2))
+            params = {
+                'batchOrders': [
+                    {
+                        'symbol': symbol,
+                        'side': 'BUY',
+                        'type': 'LIMIT',
+                        'quantity': str(quantity_l),
+                        'timeInForce': 'GTC',
+                        'price' : close_price
+                    }
+                ]
+
+            }
+            responce = send_signed_request('POST', '/fapi/v1/batchOrders', params)
+        if s_l == 'short':
+            close_price = str(round(sprice * (1 - 0.01), 2))
+            params = {
+                'batchOrders': [
+                    {
+                        'symbol': symbol,
+                        'side': 'SELL',
+                        'type': 'LIMIT',
+                        'quantity': str(quantity_l),
+                        'timeInForce': 'GTC',
+                        'price' : close_price
+                    }
+                ]
+
+            }
+            responce = send_signed_request('POST', '/fapi/v1/batchOrders', params)
 
 
-    # prepared_df[0:100][['slope']].plot()
-    # prepared_df[0:100][['close']].plot()
-    # prepared_df[0:100][['close', 'chanel_max', 'chanel_min']].plot()
-    # plt.show()
+    def close_position(symbol, s_l, quantity_l): #function closing position
+        sprice = get_symbol_price(symbol)
+
+        if s_l == 'long':
+            close_price = str(round(sprice * (1 - 0.01), 2))
+            params = {
+                        'symbol': symbol,
+                        'side': 'SELL',
+                        'type': 'LIMIT',
+                        'quantity': str(quantity_l),
+                        'timeInForce': 'GTC',
+                        'price': close_price
+                    }
+            responce = send_signed_request('POST', '/fapi/v1/batchOrders', params)
+
+        if s_l == 'short':
+            close_price = str(round(sprice * (1 + 0.01), 2))
+            params = {
+                'symbol': symbol,
+                'side': 'BUY',
+                'type': 'LIMIT',
+                'quantity': str(quantity_l),
+                'timeInForce': 'GTC',
+                'price': close_price
+            }
+            responce = send_signed_request('POST', '/fapi/v1/batchOrders', params)
+
+
+    def get_opened_positions(symbol): # function gettion information about opened prices
+        status = client.futures_account()
+        positions = pd.DataFrame(status['positions'])
+        a = positions[positions['symbol'] == symbol]['positionAmt'].astype(float).tolist()[0]
+        leverage = int(positions[positions['symbol'] == symbol]['leverage'])
+        entryprice = positions[positions['symbol'] == symbol]['entryPrice']
+        profit = float(status['totalUnrealizedProfit'])
+        balance = round(float(status['totalWalletBalance']), 2)
+        if a > 0:
+            pos = 'long'
+        elif a < 0:
+            pos = 'short'
+        else:
+            pos = ""
+        return [pos, a, profit, leverage, balance, round(float(entryprice), 3), 0]
+
+    def check_and_close_orders(symbol):
+        global isStop
+        a = client.futures_get_open_orders(symbol = symbol)
+        if len(a) > 0:
+            isStop = False
+            client.futures_cancel_all_open_orders(symbol = symbol)
+
+
+    def get_symbol_price(symbol):
+        prices = client.get_all_tickers()
+        df = pd.DataFrame(prices)
+        return float(df[df['symbol'] == symbol]['price'])
+
+
+
+
 
     lend = len(prepared_df)
     prepared_df['hcc'] = [None] * lend
